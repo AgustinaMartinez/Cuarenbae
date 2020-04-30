05879bf155bae2e86137c13066098ddfa7ca8bab
Agustina Martinez <agustina.martinez@globant.com>
2020-04-30 05:16:06 -0300

    burger menu and classes names changed

--- my-app/src/Components/header.js
+++ my-app/src/Components/header.js
@@ -1,46 +1,30 @@
 import React, {Component} from 'react';
-import logo from '../../public/Assets/logo.png';
-import bars from '../../public/Assets/bars.png';
+import Menu from './menu';
 
 class Header extends Component{
-    
-    state ={
+
+    state = {
         MenuVisible: false
     }
 
-    openAndCloseMenu = () => {
+    showMenu = () => {
         this.setState({MenuVisible: !this.state.MenuVisible})
     }
 
-    /*icon(){
-        return {
-            fontSize: '20px',
-            color: this.props.task.done ? 'grey' :'red',
-            textDecoration: this.props.task.done ? 'line-through' : 'none'
-        }
-    }
-    */
-
     render(){
+        const MenuVisible = this.state.MenuVisible;
         return(
             <header>
                 <nav className="navbar">
-                    <div className="icons">
-                        <a onClick={this.openAndCloseMenu}><img className="bars" alt="bars" src={bars}></img></a>
-                        <img className="logo" alt="logo" src={logo}></img>
+                    <button className="navbar__button" onClick={this.showMenu}>
+                        <img className="navbar__button__bars" alt="bars" src="/Assets/bars.png"></img>
+                    </button>
+                    <Menu className={`navbar${MenuVisible?'__yes':''}`}/>
+                    <img className="navbar__logo" alt="logo" src="/Assets/logo.png"></img>
+                    <div className="navbar__container">
+                        <input type="text" className="navbar__container__search" placeholder="Buscar"></input>
+                        <img src="/Assets/search_icon.svg" className="navbar__container__img" alt="search"></img>
                     </div>
-                    <ul className="navbar_menu">
-                        <li><a href="#">ARGENTINA</a></li>
-                        <li><a href="#">AMÉRICA</a></li>
-                        <li><a href="#" href="#">TENDENCIAS</a></li>
-                        <li><a href="#">DEPORTES</a></li>
-                        <li><a href="#">POLÍTICA</a></li>
-                        <li><a href="#">SOCIEDAD</a></li>
-                        <li><a href="#">TECNO</a></li>
-                        <li><a href="#">TURISMO</a></li>
-                        <li><a href="#">SALUD</a></li>
-                        <li><a href="#">CULTURA</a></li>
-                    </ul>
                 </nav>
             </header>
         )
